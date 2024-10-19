e180578fe6ead2fd6eefb4548b60de9b32fe9660
flaronin <flaronin1@gmail.com>
2024-10-19 10:22:51 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# CapCut1
+# Dev Expert
+Página de vendas
+
+Veja aqui como ficou: https://dev-expert-zeta.vercel.app
+
+## Tecnologias
+- Html
+- Css
+- Javascript
+- Scss
+- Tailwind
+- [Aos animation](https://michalsnik.github.io/aos/)
+
+## License
+[MIT](license)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#a61482",
+        primaryColorHover: "#700c57",
+        secondaryColor: "#121214",
+        thirdColor: "#202024",
+        textPrimaryColor: "#EEEEEE",
+        textSecondaryColor: "",
+      }
+    }
+  },
+  darkMode: 'class',
+}
